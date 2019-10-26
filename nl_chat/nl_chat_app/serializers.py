891ab77c634cdbf66beb6920d307d488350159ee
JCH936 <jch936@bham.ac.uk>
2019-10-26 17:54:29 +0100

    Update

--- nl_chat/nl_chat_app/serializers.py
+++ nl_chat/nl_chat_app/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-
+from .models import Message
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,3 +12,21 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+class MessageSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    recieved_time = serializers.DateTimeField(required=True, allow_blank=False)
+    sender = serializers.CharField(required=True, allow_blank=False)
+    content = serializers.CharField(allow_blank=True, required=True)
+    reply_sent = serializers.BooleanField(default=False)
+
+    def create(self, validated_data):
+        return Message.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        pass
+
+
+    # sender = models.TextField()
+    # content = models.TextField()
+    # reply_sent = models.BooleanField(default=False)
